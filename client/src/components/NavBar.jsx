de8912f3577201c3a3d688d169257ed90b448d07
Jugal <jugal@Jugals-MacBook-Air.local>
2026-01-20 12:49:21 +0530

    Initial commit - MERN blog website setup

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
+        <img src={ "" } alt="" />
+        </div>
+  )
+}
+
+export default NavBar
