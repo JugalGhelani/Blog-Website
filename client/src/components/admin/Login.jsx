c9d213d72fd6c8b91c1e0897df868fbf3eec1dcd
Jugal Ghelani <jugalghelani07@gmail.com>
2026-01-23 18:59:01 +0530

    Add admin panel components and routing

---
+++ client/src/components/admin/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full py-6 text-center">
+            <h1 className="text-3xl font-bold">
+              <span className="text-primary">Admin</span> Login
+            </h1>
+            <p class="font-light">
+              Enter your credentials to access the admin panel
+            </p>
+
+            <form
+              onSubmit={handleSubmit}
+              className="mt-8 w-full sm:max-w-md text-left text-gray-600"
+            >
+              <div className="flex flex-col">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="border-b-2 border-gray-300 p-2 outline-none mb-6"
+                  placeholder="Enter your email id"
+                  />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  required
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="border-b-2 border-gray-300 p-2 outline-none mb-6"
+                  placeholder="Enter your password"
+                />
+              </div>
+              <button
+                type="submit"
+                class="w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
